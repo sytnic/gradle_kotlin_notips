6f8952043d5c5bed96f442fd891dc3b51f1324ad
sytnic <sytnic@list.ru>
2024-04-04 16:29:18 +0300

    02_01e

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+package org.example
+
+fun main() {
+    //println("Hello World!")
+
+    var ourFirstVariable = true
+    println(ourFirstVariable)
+    ourFirstVariable = false
+    println(ourFirstVariable)
+
+    println(ourFirstVariable == true)
+    println(ourFirstVariable == false)
+}
