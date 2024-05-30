e2f3c9043fcb2ef9771279adcd392157715a21e8
sytnic <sytnic@list.ru>
2024-05-30 16:21:48 +0300

    012-Basic functions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,39 +1,8 @@
-package org.example
+
+fun printGreeting() = println("Hello Kotlin")
 
 fun main() {
-    // Значение null не может быть установлено изначально для переменной.
-    // Это вызывает ошибку
-    //var aNullableString: String = null
+    fun getGreeting() = "Hello Kotlin"
 
-    // Для присваивания null ставится знак ? после типа
-    //var aNullableString: String? = null
-    //aNullableString = "Kotlin"
-
-    // Попытка доступа к null-значению "небезопасным способом"
-    // вызывает ошибку
-    //var aNullableString: String? = null
-    //println(aNullableString.length)
-
-    // способы решения:
-
-    // 1
-    // Двойной знак !! подтверждает, что обращение идет к переменной, которая не null,
-    // или выпадет Исключение, если значение есть null
-    //var aNullableString: String? = null
-    //println(aNullableString!!.length)
-
-    // 2
-    // Знак ? поможет вывести null, если значение null.
-    // Исключение NullPointerException не будет появляться.
-    //var aNullableString: String? = null
-    //println(aNullableString?.length)
-
-    // В последнем случае чтобы не выводить на выход слово null,
-    // можно применить оператор Элвиса (Elvis) -
-    // аналог тернарного оператора для вывода запасного значения,
-    // если значение всё-таки будет null
-    var aNullableString: String? = "Kotlin" // null
-    println(aNullableString?.length ?: "the value is null")
-
-
+    println(getGreeting())
 }
