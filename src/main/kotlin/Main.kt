57b5a18c69aea5499c429b82a01831646d4de1d2
sytnic <sytnic@list.ru>
2025-02-13 11:58:42 +0300

    022-while loops

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,8 @@
-
-fun checkType(input:Any?) { // ? означает, что можно передавать ещё тип Null
-    when (input) {
-        null -> println("Input was null")
-        is String -> println("Input is a String with length ${input.length}")
-    }
-}
-
 fun main() {
-    val aGenericVariable: Any = "A string"
+    var counter = 0
 
-    checkType(aGenericVariable)
+    do {
+        println(counter)
+        counter += 1
+    } while (counter > 1)
 }
