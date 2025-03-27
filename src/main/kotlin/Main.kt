4736216b953df4b7a3172d0c744ad3efd4c7bd4a
sytnic <sytnic@list.ru>
2025-03-27 10:56:13 +0300

    5. Debugging Kotlin Code

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,18 @@
+import kotlin.random.Random
+
+fun getRandomInt() = Random.nextInt()
+
 fun main() {
-    for (i in 10 downTo 0 step 3) println(i)
+    for (i in 0..4) {
+        // создаём случайное число
+        val random = getRandomInt()
+        // является ли число чётным
+        val isEven = random.rem(2) == 1
+
+        when(isEven) {
+            true -> println("$random is even")
+            false -> println("$random is odd")
+        }
+    }
+
 }
